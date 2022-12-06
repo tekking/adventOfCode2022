2eeecc50adc34622d210d5e5944c7d52e14ea4c4
Jelco Bodewes <>
2022-12-06 17:30:54 +0100

    day 5 & 6

---
+++ Day6/Day6.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2022.Common;
+
+namespace AdventOfCode2022.Day6
+{
+    internal class Day6 : BaseDay
+    {
+        protected override void SolvePart1(string[] input)
+        {
+            foreach (var code in input)
+            {
+                Console.WriteLine(
+                    code.Select(
+                            (_, index) => (index, code[index..(index + 4)]
+                                               .ToHashSet()))
+                        .First(h => h.Item2.Count == 4)
+                        .index
+                    + 4);
+            }
+        }
+
+        protected override void SolvePart2(string[] input)
+        {
+            foreach (var code in input)
+            {
+                Console.WriteLine(
+                    code.Select(
+                            (_, index) => (index, code[index..(index + 14)]
+                                               .ToHashSet()))
+                        .First(h => h.Item2.Count == 14)
+                        .index
+                    + 14);
+            }
+        }
+    }
+}
